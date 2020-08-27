beba72a6e8953b008cda5096c66ee8a61045e25c
kmbarnett <kmbarnett94@gmail.com>
2020-08-27 14:02:00 -0600

    Updates

---
+++ precache-manifest.084606f0877973eb5ccb2cbdc70f01b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112ac327fc0622583675fc40a0682e07",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/index.html"
+  },
+  {
+    "revision": "0c50a39446f608341632",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "363b45f7c16bf9ef6157",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/static/js/2.b0512424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/static/js/2.b0512424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c50a39446f608341632",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/static/js/main.a7cbbd6d.chunk.js"
+  },
+  {
+    "revision": "203573a76061e68c77ad",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/static/js/runtime-main.08814ed3.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/mintbean-hackathon-challenge-2020-7-27/static/media/mintbean.cf18b629.png"
+  }
+]);
